194174d756508cda1673fe69aabaeb9f9ba88180
FJLouro <louro91@gmail.com>
2015-04-22 20:40:40 +0200

    Actualizacion de la version de la API
    -Modificacion de las rutas en el fichero routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,14 @@
 |
 */
 
+//Versionado de la API
+//Las rutas quedaran algo como /api/v1.0/rutas existentes....
+// /api/v1.0/fabricante/....
+
+Route::group(array('prefix'=>'api/v1.0'),function()
+{
+
+
 
 //Creamos las rutas nuevas que tendran en cuenta
 //los controllers programados en Controllers.
@@ -23,9 +31,17 @@ Route::resource('fabricantes.aviones','FabricanteAvionController',['except'=>['e
 // Ruta /aviones/... El resto de metolos los gestiona FabricanteAvion
 Route::resource('aviones','AvionController',['only'=>['index','show']]);
 
-// Ruta por defecto /
+// Ruta por defecto http://www.dominio.local/api/v1.0
 Route::get('/', function(){
 	return "Bienvenido API RESTful de Aviones";
+});
+
+}); // Fin del versionado
+
+
+//Ruta por defecto al entrar en http://www.dominio.local
+Route::get('/', function(){
+	return "<a href='http://www.dominio.local/api/v1.0/'>Por favor acceda a la version v1.0 de la API.</a>";
 });
 
 /*
